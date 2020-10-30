1d905df0233e64a4bab21ea6477e5c7dc1bde89b
Mike Chabot <mchabot@draftkings.com>
2020-10-30 12:53:18 -0400

    Update example

--- apps/test-app/src/app/app.tsx
+++ apps/test-app/src/app/app.tsx
@@ -25,13 +25,12 @@ export const App = () => {
         <h1>Welcome to test-app!</h1>
         <p>This is the main container application</p>
       </header>
-      <p>Rendered below is a library that is both:
+      <p>Rendered below is a library that is both</p>
         <ul>
           <li><a href={buildable} target="_blank">buildable</a></li>
           <li><a href={publishable} target="_blank">publishable</a></li>
         </ul>
-        The publishable library is imports a buildable library.
-      </p>
+        <p>The publishable library is imports a buildable library.</p>
       <div className="border">
         <NxPublishable />
       </div>
